c47f171dca504cec45b6f03ec50fb92e0cde6419
Roland Schilter <roli@schilter.me>
2017-08-31 16:00:11 +0100

    Use interface for timing client requests (#52)
    * Use interface for timing client requests
    
    * Change to new shfmt import path
    
    Fixes error
    ```
    package github.com/mvdan/sh/cmd/shfmt: code in directory /go/src/github.com/mvdan/sh/cmd/shfmt expects import "mvdan.cc/sh/cmd/shfmt"
    ```

--- http/client/client.go
+++ http/client/client.go
@@ -11,8 +11,13 @@ import (
 	oldcontext "golang.org/x/net/context"
 )
 
+// Requester executes an HTTP request.
+type Requester interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
 // TimeRequestHistogram performs an HTTP client request and records the duration in a histogram
-func TimeRequestHistogram(ctx context.Context, operation string, metric *prometheus.HistogramVec, client *http.Client, request *http.Request) (*http.Response, error) {
+func TimeRequestHistogram(ctx context.Context, operation string, metric *prometheus.HistogramVec, client Requester, request *http.Request) (*http.Response, error) {
 	var response *http.Response
 	doRequest := func(_ oldcontext.Context) error {
 		var err error
