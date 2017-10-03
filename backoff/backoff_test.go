c4d0f9c68231be78d226a100d551990a236e33f1
Marc CARRE <carre.marc@gmail.com>
2017-10-03 15:52:50 +0100

    Fix casing in logrus imports.
    This fixes:
    ```
    middleware/grpc_logging.go:10:2: case-insensitive import collision: "github.com/weaveworks/common/vendor/github.com/Sirupsen/logrus" and "github.com/weaveworks/common/vendor/github.com/sirupsen/logrus"
    ```
    Changelog:
    - Changed `import` statements from `Sirupsen/logrus` to `sirupsen/logrus`, as per the official documentation: https://github.com/sirupsen/logrus
    - Ran:
    ```
    dep ensure && dep prune
    ```

--- backoff/backoff_test.go
+++ backoff/backoff_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
-	"github.com/Sirupsen/logrus/hooks/test"
+	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/weaveworks/common/backoff"
 )
 
