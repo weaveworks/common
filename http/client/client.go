03397d471f02a422c623c538edd73baca558326c
Roland Schilter <roli@schilter.me>
2017-10-18 17:31:25 +0100

    Add `client.TimedClient` implementing `Requester` (#74)
    * Add `client.TimedClient` implementing `Requester`
    
    This helps in chaining clients doing requests. Such as
    
        client.NewTimedClient(http.DefaultClient, someCollector}
    
    * Code cleanup, add tests
    
    - Return concrete type
    - Use custom type for context key
    - Add tests

--- http/client/client.go
+++ http/client/client.go
@@ -16,6 +16,39 @@ type Requester interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
+// TimedClient instruments a request. It implements Requester.
+type TimedClient struct {
+	client    Requester
+	collector instrument.Collector
+}
+
+type contextKey int
+
+// OperationNameContextKey specifies the operation name location within the context
+// for instrumentation.
+const OperationNameContextKey contextKey = 0
+
+// NewTimedClient creates a Requester that instruments requests on `client`.
+func NewTimedClient(client Requester, collector instrument.Collector) *TimedClient {
+	return &TimedClient{
+		client:    client,
+		collector: collector,
+	}
+}
+
+// Do executes the request.
+func (c TimedClient) Do(r *http.Request) (*http.Response, error) {
+	return TimeRequest(r.Context(), c.operationName(r), c.collector, c.client, r)
+}
+
+func (c TimedClient) operationName(r *http.Request) string {
+	operation, _ := r.Context().Value(OperationNameContextKey).(string)
+	if operation == "" {
+		operation = r.URL.Path
+	}
+	return operation
+}
+
 // TimeRequest performs an HTTP client request and records the duration in a histogram.
 func TimeRequest(ctx context.Context, operation string, coll instrument.Collector, client Requester, request *http.Request) (*http.Response, error) {
 	var response *http.Response
