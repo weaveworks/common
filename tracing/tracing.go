99cba494b02096fe33c3901cca2648db4ce9984b
Cody Boggs <cody@freshtracks.io>
2018-02-13 15:44:42 -0700

    collapse tracing bits to tracing.New(string) to reduce some duplication across cortex components

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -5,23 +5,30 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strconv"
 
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
 
 // New registers Jaeger as the OpenTracing implementation.
 // If jaegerAgentHost is an empty string, tracing is disabled.
-func New(jaegerAgentHost, serviceName, samplerType string, samplerParam float64) io.Closer {
+// Values are retrieved from environment variables, and are
+//  configurable per Cortex component
+func New(serviceName string) io.Closer {
+	jaegerAgentHost := os.Getenv("JAEGER_AGENT_HOST")
+	jaegerSamplerType := os.Getenv("JAEGER_SAMPLER_TYPE")
+	jaegerSamplerParam, _ := strconv.ParseFloat(os.Getenv("JAEGER_SAMPLER_PARAM"), 64)
+
 	if jaegerAgentHost != "" {
-		if samplerType == "" || samplerParam == 0 {
-			samplerType = "ratelimiting"
-			samplerParam = 10.0
+		if jaegerSamplerType == "" || jaegerSamplerParam == 0 {
+			jaegerSamplerType = "ratelimiting"
+			jaegerSamplerParam = 10.0
 		}
 		cfg := jaegercfg.Configuration{
 			Sampler: &jaegercfg.SamplerConfig{
 				SamplingServerURL: fmt.Sprintf("http://%s:5778/sampling", jaegerAgentHost),
-				Type:              samplerType,
-				Param:             samplerParam,
+				Type:              jaegerSamplerType,
+				Param:             jaegerSamplerParam,
 			},
 			Reporter: &jaegercfg.ReporterConfig{
 				LocalAgentHostPort: fmt.Sprintf("%s:6831", jaegerAgentHost),
