ed6819b7603030a18fc24bec7dbfbce39f318a05
DylanGuedes <djmgguedes@gmail.com>
2023-05-05 12:20:38 -0300

    Remove YAML definition for `disable_request_success_log`.
    - One can configure this through adding a flag on its program.

--- server/server.go
+++ server/server.go
@@ -83,7 +83,7 @@ type Config struct {
 
 	RegisterInstrumentation  bool `yaml:"register_instrumentation"`
 	ExcludeRequestInLog      bool `yaml:"-"`
-	DisableRequestSuccessLog bool `yaml:"disable_request_success_log"`
+	DisableRequestSuccessLog bool `yaml:"-"`
 
 	ServerGracefulShutdownTimeout time.Duration `yaml:"graceful_shutdown_timeout"`
 	HTTPServerReadTimeout         time.Duration `yaml:"http_server_read_timeout"`
